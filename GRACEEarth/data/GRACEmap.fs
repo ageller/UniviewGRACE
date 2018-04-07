911560d561320533470b900d7f161b5fb6533552
Aaron Geller <a-geller@northwestern.edu>
2018-04-07 13:53:03 -0500

    fixed colorspace issue in conf

--- GRACEEarth/data/GRACEmap.fs
+++ GRACEEarth/data/GRACEmap.fs
@@ -10,15 +10,17 @@ uniform sampler2D coast;
 uniform float simUseTime;
 uniform bool simBindRealtime;
 uniform bool simBindLimits;
+uniform bool simSeasonal;
 
 uniform vec3 p1min;
 uniform vec3 p1max;
 uniform vec3 p2min;
 uniform vec3 p2max;
-uniform float cmin;
-uniform float cmax;
+uniform float clim;
 uniform float t0;
 uniform float tf;
+uniform vec3 minColor;
+uniform vec3 maxColor;
 
 in vec2 TexCoord;
 
@@ -30,9 +32,9 @@ void main()
 {	
 	vec3 p1t = texture(fit1,TexCoord).rgb;
 	vec3 p2t = texture(fit2,TexCoord).rgb;
-	vec4 coastLines = texture(coast,TexCoord);
+		vec4 coastLines = texture(coast,TexCoord);
 	
-	vec3 p1 = p1t * (p1max - p1min) + p1min;
+	vec3 p1 = p1t * (p1max - p1min) + p1min;	
 	vec3 p2 = p2t * (p2max - p2min) + p2min;
 	
 	float dayfract = uv_simulationtimeSeconds/(24.0*3600.0);
@@ -47,23 +49,27 @@ void main()
 	if (simBindLimits){
 		t = clamp(t, 0., tf - t0);
 	}
-	
+
 	float f1 = p1.r*pow(t, 3.) + p1.g*pow(t, 2.) + p1.b*t + p2.b;
-	float f2 = p2.r*sin(2.*PI*(t + p2.g));
-	
+	float f10 = p2.b;
+
+	float f2 = 0.;
+	float f20 = 0.;
+	if (simSeasonal){
+		f2 = p2.r*sin(2.*PI*(t + p2.g));
+		f20 = p2.r*sin(2.*PI*p2.g);
+	}
 	float cmwe = f1 + f2;
-	float cmwe0 = p2.b + p2.r*sin(2.*PI*p2.g);
+	float cmwe0 = f10 + f20;
 
-	cmwe = cmwe - cmwe0;
-
-	float cmweN = (cmwe - cmin) / (cmax - cmin);
+	cmwe = cmwe;// - cmwe0;
 
 	vec4 GRACEcolor = vec4(1.);
 	if (cmwe > 0.){
-		GRACEcolor.rgb = mix(vec3(1.,1., 1.), vec3(0.,0.,1.), clamp(cmwe/cmax,0.,1.));
+		GRACEcolor.rgb = mix(vec3(1.,1., 1.), maxColor, clamp(cmwe/clim,0.,1.));
 	} 
 	if (cmwe < 0.){
-		GRACEcolor.rgb = mix(vec3(1.,1., 1.), vec3(1.,0.,0.), clamp(cmwe/cmin,0.,1.));
+		GRACEcolor.rgb = mix(vec3(1.,1., 1.), minColor, clamp(-1.*cmwe/clim,0.,1.));
 
 	}
 	
