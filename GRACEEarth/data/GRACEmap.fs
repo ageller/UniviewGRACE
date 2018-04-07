376d1bda61a0ab983f3af71b32695d67bcda1d57
Aaron Geller <a-geller@northwestern.edu>
2018-04-07 15:53:33 -0500

    new fit images and new shaders

--- GRACEEarth/data/GRACEmap.fs
+++ GRACEEarth/data/GRACEmap.fs
@@ -12,17 +12,18 @@ uniform bool simBindRealtime;
 uniform bool simBindLimits;
 uniform bool simSeasonal;
 
-uniform vec3 p1min;
-uniform vec3 p1max;
-uniform vec3 p2min;
-uniform vec3 p2max;
+uniform vec4 p1min;
+uniform vec4 p1max;
+uniform vec4 p2min;
+uniform vec4 p2max;
 uniform float clim;
 uniform float t0;
 uniform float tf;
 uniform vec3 minColor;
 uniform vec3 maxColor;
+uniform vec3 centerColor;
 
-in vec2 TexCoord;
+in vec3 Pos;
 
 out vec4 FragColor;
 
@@ -30,12 +31,16 @@ const float PI = 3.1415926535897932384626433;
 
 void main()
 {	
-	vec3 p1t = texture(fit1,TexCoord).rgb;
-	vec3 p2t = texture(fit2,TexCoord).rgb;
-		vec4 coastLines = texture(coast,TexCoord);
+ 
+	vec3 positionOnUnitSphere = normalize(Pos)*vec3(-1,1,1);
+	vec2 TexCoord = vec2 (atan(positionOnUnitSphere.x,positionOnUnitSphere.y)/(2*PI)-.25,1-acos(positionOnUnitSphere.z)/PI);
+	vec2 GradTexCoord = vec2 (atan(abs(positionOnUnitSphere.x),positionOnUnitSphere.y)/(2*PI),1-acos(positionOnUnitSphere.z)/PI);
+	vec4 p1t = textureGrad(fit1,TexCoord, dFdx(GradTexCoord), dFdy(GradTexCoord));
+	vec4 p2t = textureGrad(fit2,TexCoord, dFdx(GradTexCoord), dFdy(GradTexCoord));
+	vec4 coastLines = textureGrad(coast,TexCoord, dFdx(GradTexCoord), dFdy(GradTexCoord));
 	
-	vec3 p1 = p1t * (p1max - p1min) + p1min;	
-	vec3 p2 = p2t * (p2max - p2min) + p2min;
+	vec4 p1 = p1t * (p1max - p1min) + p1min;	
+	vec4 p2 = p2t * (p2max - p2min) + p2min;
 	
 	float dayfract = uv_simulationtimeSeconds/(24.0*3600.0);
 	float yrs = 365.2425;
@@ -50,8 +55,8 @@ void main()
 		t = clamp(t, 0., tf - t0);
 	}
 
-	float f1 = p1.r*pow(t, 3.) + p1.g*pow(t, 2.) + p1.b*t + p2.b;
-	float f10 = p2.b;
+	float f1 = p1.r*pow(t, 3.) + p1.g*pow(t, 2.) + p1.b*t + p1.a;
+	float f10 = p1.a;
 
 	float f2 = 0.;
 	float f20 = 0.;
@@ -64,12 +69,12 @@ void main()
 
 	cmwe = cmwe;// - cmwe0;
 
-	vec4 GRACEcolor = vec4(1.);
+	vec4 GRACEcolor = vec4(centerColor, 1.);
 	if (cmwe > 0.){
-		GRACEcolor.rgb = mix(vec3(1.,1., 1.), maxColor, clamp(cmwe/clim,0.,1.));
+		GRACEcolor.rgb = mix(centerColor, maxColor, clamp(cmwe/clim,0.,1.));
 	} 
 	if (cmwe < 0.){
-		GRACEcolor.rgb = mix(vec3(1.,1., 1.), minColor, clamp(-1.*cmwe/clim,0.,1.));
+		GRACEcolor.rgb = mix(centerColor, minColor, clamp(-1.*cmwe/clim,0.,1.));
 
 	}
 	
@@ -78,4 +83,5 @@ void main()
 	//GRACEcolor.b = cmweN;
 		
 	FragColor = GRACEcolor * coastLines;
+	//FragColor = 1e-9*FragColor + vec4(positionOnUnitSphere,1);
 }
--- GRACEEarth/data/GRACEmap.vs
+++ GRACEEarth/data/GRACEmap.vs
@@ -2,16 +2,10 @@ in vec3 uv_vertexAttrib;
 in vec2 uv_texCoordAttrib0;
 uniform mat4 uv_modelViewProjectionMatrix;
 
-out vec2 TexCoord;
-
-const mat3 RotMat = mat3(1,  0, 0,
-						 0,  0, 1,
-						 0, -1, 0);
-
-
+out vec3 Pos;
 
 void main()
 {  
-  gl_Position = uv_modelViewProjectionMatrix*vec4(0.98*RotMat*uv_vertexAttrib,1.0);
-  TexCoord = vec2(0.75-uv_texCoordAttrib0.x,uv_texCoordAttrib0.y);
+  Pos = 0.98 * uv_vertexAttrib;
+  gl_Position = uv_modelViewProjectionMatrix*vec4(Pos,1.0);
 }
